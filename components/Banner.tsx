f9205f4ea58aee744ed6cf806ddccd18e8324b73
Hishaam <hishaamtchirou73@gmail.com>
2023-05-05 23:34:34 +0000

    fix banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,13 +1,13 @@
 export function Banner () {
     return (
         <div className="flex justify-evenly p-4 mx-auto">
-            <div className="space-y-4">
-                <h1 className="text-5xl text-darkBlue">Lorem ipsum dolor sit amet</h1>
-                <p>consectetur adipisicing elit. Ducimus ut saepe sed aliquid, sequi, necessitatibus repellat ipsa labore repellendus quos molestias dolorem</p>
-                <button>Join now</button>
+            <div className="ml-20 mt-32 space-y-8 w-4/6">
+                <h1 className="text-7xl text-darkBlue">Lorem ipsum dolor sit amet</h1>
+                <p className="text-darkBlue text-md">consectetur adipisicing elit. Ducimus ut saepe sed aliquid, sequi, necessitatibus repellat ipsa labore repellendus quos molestias dolorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nesciunt, a mollitia iste voluptate odio nostrum</p>
+                <button className="text-yellow px-8 py-4 bg-pink text-4xl rounded-md">Join now</button>
             </div>
-            <div className="">
-                <img src="../images/cercle.png" alt="image en tête" className="w-3/4" />
+            <div className="mr-20">
+                <img src="../images/cercle.png" alt="image en tête" className="" />
             </div>
         </div>
     );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,18 @@
-export function Navbar () {
-    return (
-        <div className="flex justify-between items-center p-4">
-        <a href="">
-          <img src="../images/logo.png" alt="logo" className="" />
-        </a>
-        <div className="flex space-x-4 text-3xl font-semibold text-pink">
-          <a href="" className="hover:underline">About</a>
-          <a href="" className="hover:underline">Calendar</a>
-          <a href="" className="hover:underline">Past events</a>
-          <a href="" className="hover:underline">Our team</a>
-        </div>
-        <a href="" className="">
-          <img src="../login.svg" alt="" />
-        </a>
+export function Navbar() {
+  return (
+    <div className="w-[90%] mx-auto flex justify-between items-center p-4">
+      <a href="">
+        <img src="../images/logo.png" alt="logo" className="w-1/2" />
+      </a>
+      <div className="flex space-x-6 text-2xl font-semibold text-pink">
+        <a href="" className="hover:underline">About</a>
+        <a href="" className="hover:underline">Calendar</a>
+        <a href="" className="hover:underline">Past events</a>
+        <a href="" className="hover:underline">Our team</a>
       </div>
-    );
+      <a href="" className="">
+        <img src="../login.svg" alt="" className="w-1/2" />
+      </a>
+    </div>
+  );
 }
