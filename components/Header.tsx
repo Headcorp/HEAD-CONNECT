b95a8240c5dbd1eca98b4afbda72963825fbe760
Hishaam <hishaamtchirou73@gmail.com>
2023-05-11 20:30:10 +0000

    fix: hydration error

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,20 @@
+import { useState, useEffect} from 'react';
 import { useMediaQuery } from 'react-responsive';
-import {Navbar} from './Navbar'
-import {Banner} from './Banner'
-import { NavbarMobile } from './NavbarMobile'
+import {Navbar} from './Navbar';
+import {Banner} from './Banner';
+import { NavbarMobile } from './NavbarMobile';
 
 export function Header () {
-  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const [isMobile, setIsMobile] = useState(false)
   // const handleNavbar = () => {
   //   return isMobile ? <NavbarMobile /> : <Navbar />;
   // }
+  const tempIsMobile = useMediaQuery({ maxWidth: 768 })
 
+  useEffect(() => {
+    setIsMobile(tempIsMobile)
+  }, [])
+  
   return (
     <div className="header">
         {/* {handleNavbar()} */}
